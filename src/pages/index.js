845e4633fdffd778c4c828ba28a93b40d9385a8e
Ali Bashi <alisalad123@gmail.com>
2018-12-05 15:44:49 +1100

    changed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,12 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Gatsby demo saite</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
